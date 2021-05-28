845c2842868a5863f26cf1f5c0b832957d150797
Lalit Sharma <dev.lalit.sh@gmail.com>
2021-05-28 11:55:58 +0530

    Revert "dev merged"
    This reverts commit 9d34476e97c0f5efd89270f2df056ae3ed8a34dd, reversing
    changes made to 7bec522e4654eb79bfd6447ba9486b05fa7ab652.

--- src/modules/Home/modules/camera.js
+++ src/modules/Home/modules/camera.js
@@ -192,6 +192,7 @@ class Camera extends PureComponent {
         }
     }
 
+
     render() {
         let height = this.getCameraViewHeight();
         let previewDimensions = this.getPreviewDimensions();
